2ac784c50853c3bedc9918c83522f54adff06269
gaetbout <gaetant.ansotte@hotmail.com>
2022-11-08 21:16:03 +0100

    handling multiple logs, still ugly but got to tests

--- src/views/L1L2.vue
+++ src/views/L1L2.vue
@@ -15,6 +15,11 @@
                     Transaction hash:
                     <input v-model="txHash" type="text" class="form-control formy my-3 mr-2  shadow"
                         placeholder="0x035ee021f94d527939c991b0ee27023046fbe218483befb350326bcb935831d6" />
+                    <div v-if="shouldAskLogIndex">
+                        Log index (between 1 and {{ numberOfLogs }}):
+                        <input v-model="logIndex" type="number" class="form-control formy my-3 mr-2  shadow"
+                            placeholder="1" />
+                    </div>
                     <button class="btn btn-sm btn-success" @click="test">
                         Retrieve info <i class="fa fa-external-link" aria-hidden="true"></i>
                     </button>
@@ -68,13 +73,17 @@ export default {
             entryPointSelector: "",
             nonce: "",
             callData: "",
-            isMainnet: false
+            isMainnet: false,
+            shouldAskLogIndex: false,
+            logIndex: 1,
+            numberOfLogs: "",
         };
     },
     methods: {
         computeAndOpenL2Tx() {
             // TODO si erreur ==> log some message like "Check network"
             const allCalldata = this.callData.split(",");
+            const chaindId = this.isMainnet ? constants.StarknetChainId.MAINNET : constants.StarknetChainId.TESTNET;
             allCalldata.unshift(this.toAddress);
             const url = this.isMainnet ? "https://starkscan.co/tx/" : "https://testnet.starkscan.co/tx/";
             const txHash = hash.calculateTransactionHashCommon(
@@ -84,7 +93,7 @@ export default {
                 this.entryPointSelector,
                 allCalldata, // toAddress + calldata
                 "0", // maxFee
-                this.chaindId, // chainId
+                chaindId, // chainId
                 [this.nonce]
             );
             window.open(url + txHash, '_blank')
@@ -94,19 +103,21 @@ export default {
             let web3 = this.getProvider();
             if (!web3) return;
             let result = await web3.eth.getTransactionReceipt(this.txHash);
-            console.log(result);
             const log2Selector = "0x9592d37825c744e33fa80c469683bbd04d336241bb600b574758efd182abe26a"; // ConsumedMessageToL2 selector
             let allLog2Logs = result.logs.filter(log => log.topics[0] == log2Selector);
-            if (allLog2Logs.length > 1) {
-                console.log(allLog2Logs);
+            if (allLog2Logs.length > 1 && !this.shouldAskLogIndex) {
+                this.shouldAskLogIndex = true;
                 return;
             }
-            let currentLog = allLog2Logs[0];
-            this.contractAddress = currentLog.topics[2];
+            if (this.logIndex > allLog2Logs.length) {
+                alert("Log index incorrect");
+                return;
+            }
+            let currentLog = allLog2Logs[this.logIndex - 1];
             this.toAddress = currentLog.topics[1];
+            this.contractAddress = currentLog.topics[2];
             this.entryPointSelector = currentLog.topics[3];
 
-            // this.nonce = result.nonce;
             let data = web3.eth.abi.decodeLog([{
                 type: 'uint256[]',
                 name: 'payload'
@@ -123,7 +134,6 @@ export default {
                 alert("Please install metamask");
                 return;
             }
-
             if (this.isMainnet && window.ethereum.chainId == "0x5") {
                 alert("Please switch metamask to mainnet");
                 return;
@@ -133,7 +143,7 @@ export default {
                 return;
             }
             return new Web3(window.ethereum);
-        }
+        },
     }
 }
 
