677d8e203f3c0c43a1cf36bf395173f2a4ee7fd0
gaetbout <gaetant.ansotte@hotmail.com>
2022-11-08 20:51:29 +0100

    handle mainnet && goerli

--- src/views/L1L2.vue
+++ src/views/L1L2.vue
@@ -11,6 +11,7 @@
         <div class="card-body px-lg-5">
             <ul class="list-group mb-4">
                 <li class="list-group-item">
+                    <Toggle class="float-right togglePar" v-model="isMainnet" onLabel="Mainnet" offLabel="Testnet" />
                     Transaction hash:
                     <input v-model="txHash" type="text" class="form-control formy my-3 mr-2  shadow"
                         placeholder="0x035ee021f94d527939c991b0ee27023046fbe218483befb350326bcb935831d6" />
@@ -20,7 +21,6 @@
                 </li>
                 <li class="list-group-item">
                     Contract address:
-                    <Toggle class="float-right togglePar" v-model="isMainnet" onLabel="Mainnet" offLabel="Testnet" />
                     <input v-model="contractAddress" type="text" class="form-control formy my-3 mr-2  shadow"
                         placeholder="0x035ee021f94d527939c991b0ee27023046fbe218483befb350326bcb935831d6" />
                     To address:
@@ -34,7 +34,6 @@
                     Calldata (separate each value using ","):
                     <input v-model="callData" type="text" class="form-control formy my-3 mr-2  shadow"
                         placeholder="a, b, c, ..." />
-
                     <!-- 1393428179030720295440092695193628168230707649901849797435563042612822742693, 11819812303435348947619, 0 -->
                     Nonce:
                     <input v-model="nonce" type="number" class="form-control formy my-3 mr-2  shadow"
@@ -52,6 +51,7 @@
 
 <script>
 import { hash } from "starknet";
+import Web3 from 'web3'
 import { constants } from "starknet";
 import Toggle from "@vueform/toggle";
 
@@ -62,6 +62,7 @@ export default {
     },
     data() {
         return {
+            txHash: "",
             contractAddress: "",
             toAddress: "",
             entryPointSelector: "",
@@ -72,9 +73,9 @@ export default {
     },
     methods: {
         computeAndOpenL2Tx() {
+            // TODO si erreur ==> log some message like "Check network"
             const allCalldata = this.callData.split(",");
             allCalldata.unshift(this.toAddress);
-            const chaindId = this.isMainnet ? constants.StarknetChainId.MAINNET : constants.StarknetChainId.TESTNET;
             const url = this.isMainnet ? "https://starkscan.co/tx/" : "https://testnet.starkscan.co/tx/";
             const txHash = hash.calculateTransactionHashCommon(
                 constants.TransactionHashPrefix.L1_HANDLER, // txHashPrefix
@@ -83,13 +84,55 @@ export default {
                 this.entryPointSelector,
                 allCalldata, // toAddress + calldata
                 "0", // maxFee
-                chaindId, // chainId
+                this.chaindId, // chainId
                 [this.nonce]
             );
             window.open(url + txHash, '_blank')
         },
-        test() {
+        async test() {
+            if (!this.txHash) return;
+            let web3 = this.getProvider();
+            if (!web3) return;
+            let result = await web3.eth.getTransactionReceipt(this.txHash);
+            console.log(result);
+            const log2Selector = "0x9592d37825c744e33fa80c469683bbd04d336241bb600b574758efd182abe26a"; // ConsumedMessageToL2 selector
+            let allLog2Logs = result.logs.filter(log => log.topics[0] == log2Selector);
+            if (allLog2Logs.length > 1) {
+                console.log(allLog2Logs);
+                return;
+            }
+            let currentLog = allLog2Logs[0];
+            this.contractAddress = currentLog.topics[2];
+            this.toAddress = currentLog.topics[1];
+            this.entryPointSelector = currentLog.topics[3];
 
+            // this.nonce = result.nonce;
+            let data = web3.eth.abi.decodeLog([{
+                type: 'uint256[]',
+                name: 'payload'
+            }, {
+                type: 'uint256',
+                name: 'nonce'
+            }], currentLog.data)
+            this.callData = data.payload.toString();
+            this.nonce = data.nonce;
+        },
+        getProvider() {
+            // TODO better handle this
+            if (!window.ethereum) {
+                alert("Please install metamask");
+                return;
+            }
+
+            if (this.isMainnet && window.ethereum.chainId == "0x5") {
+                alert("Please switch metamask to mainnet");
+                return;
+            }
+            if (!this.isMainnet && window.ethereum.chainId == "0x1") {
+                alert("Please switch metamask to goerli");
+                return;
+            }
+            return new Web3(window.ethereum);
         }
     }
 }
