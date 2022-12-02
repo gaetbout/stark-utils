b480fc60be4d8ef7f2bf2ab43cf7f34963e62003
gaetbout <gaetant.ansotte@hotmail.com>
2022-12-02 21:18:16 +0100

    push to trigger actions

--- src/views/L1L2.vue
+++ src/views/L1L2.vue
@@ -11,31 +11,20 @@
     https://alpha4.starknet.io/feeder_gateway/get_transaction?transactionHash=0x56b7c13c0008f78d4cb4e39f993d7ed9921bf140f54966574d84d9c7dd80702
     https://docs.starknet.io/documentation/develop/L1-L2_Communication/messaging-mechanism/
     -->
+
     <div class="card-body px-lg-5">
       <div v-if="hasMetamask()">
         <div class="title">
           <h2>This only works with ethereum Mainnet and Goerli</h2>
         </div>
-        <ExpandableSection
-          @toggle="toggleSections"
-          :isOpened="firstSectionOpened"
-          title="Automated input (easy mode)"
-        />
-        <div
-          class="collapsedSection"
-          :class="{ fullSize1: firstSectionOpened }"
-        >
+        <ExpandableSection @toggle="toggleSections" :isOpened="firstSectionOpened"
+          title="Automated input (easy mode)" />
+        <div class="collapsedSection" :class="{ fullSize1: firstSectionOpened }">
           <AutomatedInput :isMainnet="isMainnet()" />
         </div>
-        <ExpandableSection
-          @toggle="toggleSections"
-          :isOpened="!firstSectionOpened"
-          title="Manual input (advanced mode)"
-        />
-        <div
-          class="collapsedSection"
-          :class="{ fullSize2: !firstSectionOpened }"
-        >
+        <ExpandableSection @toggle="toggleSections" :isOpened="!firstSectionOpened"
+          title="Manual input (advanced mode)" />
+        <div class="collapsedSection" :class="{ fullSize2: !firstSectionOpened }">
           <ManualInput :isMainnet="isMainnet()" />
         </div>
       </div>
