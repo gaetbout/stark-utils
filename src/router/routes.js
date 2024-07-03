fea6c76c0f4fe829844df4822e142f41865f55d6
gaetbout <gaetan.ansotte@hotmail.com>
2024-07-03 20:27:13 +0200

    revert

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,7 +1,6 @@
 import Home from '@/views/Home.vue'
 import Converter from '@/views/Converter.vue'
-import L1L2 from '@/views/L1L2.vue'
-import EC from '@/views/EC.vue'
+import Hash from '@/views/Hash.vue'
 import NotFound from '@/views/notFound.vue'
 
 const routes = [
@@ -16,14 +15,9 @@ const routes = [
         component: Converter,
     },
     {
-        path: '/l1l2',
-        name: 'L1L2',
-        component: L1L2,
-    },
-    {
-        path: '/signature',
-        name: 'Elliptic curve',
-        component: EC,
+        path: '/hash',
+        name: 'Hash',
+        component: Hash,
     },
     {
         path: '/:pathMatch(.*)*',
